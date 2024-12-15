b6adf652134e5272dfccf61d204910536049376b
Ritesh Sajwan <riteshsaj8958@gmail.com>
2024-12-15 23:50:48 +0530

    [ADD] changes floder structure and some ts config

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import express from 'express'
 import App from './services/ExpressApp'
 import dbConnection from './services/Database'
 import "reflect-metadata";
-
+const PORT = process.env.PORT
 
 const StartServer = async()=>{
     const app = express();
     await dbConnection();
     await App(app);
-    app.listen(8000,()=>{
-        console.log("Server Listing to port 8000")
+    app.listen(PORT,()=>{
+        console.log(`Server Listing to port ${PORT}`)
     })
 }
 
--- src/services/ExpressApp.ts
+++ src/services/ExpressApp.ts
@@ -1,13 +1,10 @@
 import  express, { Application } from 'express'
-import bodyParser from 'body-parser';
 import { AdminRoute , CustomerRoute, ShoppingRoute, VandorRoute } from '../routes';
 import path from 'path';
 
 export default async(app:Application)=>{
-
-
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended:true})) //for multi part file data
+    app.use(express.json());
+    app.use(express.urlencoded({extended:true})) //for multi part file data
     app.use('/images', express.static(path.join(__dirname, 'images')));
 
     app.use('/admin',AdminRoute)
